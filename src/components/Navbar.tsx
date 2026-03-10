a46b06d3b0b1b29bba83c476b45a301b1520952f
robotraderai <robotraderai@gmail.com>
2026-03-10 12:58:09 -0400

    Fix Telegram Bot Username and login widget placement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     if (!user) {
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?22";
-        script.setAttribute('data-telegram-login', "DakyBettingBot"); // Chanje sa ak non bot ou an
+        script.setAttribute('data-telegram-login', "Daky_400_day_bot"); // CHWAZI NON BOT REYÈL LA
         script.setAttribute('data-size', 'medium');
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
         script.setAttribute('data-request-access', 'write');
